bd9ea9cf6d9588eec24490cc25c6d89b58c3d9eb
Mehmet Munaf <moh.munaf@gmail.com>
2020-04-03 01:26:23 +0300

    Home page adhestments.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,7 +13,7 @@
                 <div class="uk-flex uk-flex-center uk-padding" uk-grid>
                     <div class="uk-card uk-card-default uk-card-body uk-width-2-3@m uk-padding">
                         <div class="uk-child-width-expand@s" uk-grid>
-                            <div>
+                            <div class="uk-flex uk-flex-middle uk-text-center">
                                 <div class="uk-text-center">
                                     <img src="{{asset_image('/assets/true_friends.png')}}" width="300">
                                 </div>
@@ -22,7 +22,7 @@
                                 <div class="uk-flex-center uk-text-center" style="padding-top: 20px">
                                     <div>
                                         <h3 class="uk-card-title uk-text-primary">Welcome to {{getApplicationDomain()}}</h3>
-                                        <p>
+                                        <p style="margin: 0px; padding: 0px; text-align: justify; text-justify: inter-word">
                                             {{getSite()->description}}
                                         </p>
                                     </div>

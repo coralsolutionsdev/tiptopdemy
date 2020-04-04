90d0d2e55e940f811a01f9cb96fd8ffa5f42e75f
Mehmet Munaf <moh.munaf@gmail.com>
2020-04-04 13:41:29 +0300

    Revert "Patch to get tag package to work with MySQL 5.6."
    This reverts commit 04ac773b112b2e75d3a72701137e4f3b641acf49.

--- vendor/spatie/laravel-tags/src/Tag.php
+++ vendor/spatie/laravel-tags/src/Tag.php
@@ -17,17 +17,6 @@ class Tag extends Model implements Sortable
 
     public $guarded = [];
 
-    /**
-     * The attributes that should be cast to native types.
-     * Added this to support text type instead of json for MySQL 5.6 and lower
-     *
-     * @var array
-     */
-    protected $casts = [
-        'name' => 'array',
-        'slug' => 'array'
-    ];
-
     public function scopeWithType(Builder $query, string $type = null): Builder
     {
         if (is_null($type)) {
@@ -69,6 +58,7 @@ class Tag extends Model implements Sortable
     public static function getWithType(string $type): DbCollection
     {
         return static::withType($type)->orderBy('order_column')->get();
+//        return static::withType($type)->orderBy('order_column')->where(\DB::raw( "json_extract(name, '$." . $locale . "')" ), '=', $name)->get();
     }
 
     public static function findFromString(string $name, string $type = null, string $locale = null)
@@ -76,7 +66,7 @@ class Tag extends Model implements Sortable
         $locale = $locale ?? app()->getLocale();
 
         return static::query()
-            ->where("name", json_encode(array($locale => $name)))
+            ->where("name->{$locale}", $name)
             ->where('type', $type)
             ->first();
     }
@@ -86,7 +76,7 @@ class Tag extends Model implements Sortable
         $locale = $locale ?? app()->getLocale();
 
         return static::query()
-            ->where("name", json_encode(array($locale => $name)))
+            ->where("name->{$locale}", $name)
             ->first();
     }
 

fc5c6617b2ab2d8d584ac8d11113dac888011220
Mehmet Munaf <moh.munaf@gmail.com>
2020-04-05 21:55:23 +0300

    Countries: add countries controller.

---
+++ app/Http/Controllers/System/CountriesController.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Http\Controllers\System;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use PragmaRX\Countries\Package\Countries;
+class CountriesController extends Controller
+{
+    protected $breadcrumb;
+    protected $page_title;
+
+    public function __construct()
+    {
+        $this->page_title = 'Languages';
+        $this->breadcrumb = [
+            'Languages' => '',
+        ];
+    }
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $page_title =  $this->page_title;
+        $breadcrumb =  $this->breadcrumb;
+        $countries = new Countries();
+        $countries =  $countries->all()->sortBy('name.common');
+        return view('system.countries.index', compact('page_title','breadcrumb', 'countries'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}

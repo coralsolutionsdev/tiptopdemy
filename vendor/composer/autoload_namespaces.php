c0654949a197ecffc6ffd3871cc49322ff1ab5e2
Mehmet Munaf <moh.munaf@gmail.com>
2020-04-18 22:55:19 +0300

    [System] add log viewer

--- vendor/composer/autoload_namespaces.php
+++ vendor/composer/autoload_namespaces.php
@@ -8,6 +8,7 @@ $baseDir = dirname($vendorDir);
 return array(
     'Webpatser\\Countries' => array($vendorDir . '/webpatser/laravel-countries/src'),
     'UpdateHelper\\' => array($vendorDir . '/kylekatarnls/update-helper/src'),
+    'Rap2hpoutre\\LaravelLogViewer\\' => array($vendorDir . '/rap2hpoutre/laravel-log-viewer/src'),
     'Parsedown' => array($vendorDir . '/erusev/parsedown'),
     'Mockery' => array($vendorDir . '/mockery/mockery/library'),
 );

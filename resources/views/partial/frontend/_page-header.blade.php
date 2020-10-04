48f1dd4321f6806ee2b4b9d86d0380f44f8b140a
Mehmet Munaf <moh.munaf@gmail.com>
2020-10-04 08:03:33 +0300

    [TinyMCE] enable local plugin access [BugFix] fix error $model name cannot be found

--- resources/views/partial/frontend/_page-header.blade.php
+++ resources/views/partial/frontend/_page-header.blade.php
@@ -2,7 +2,7 @@
     <div class="uk-grid">
         <div class="uk-width-expand">
             <div style="margin-bottom: -5px">
-                <p style="margin: 0 0 -5px 0">{{__('main.'.$modelName)}}</p>
+                <p style="margin: 0 0 -5px 0">{{!empty($modelName) ? __('main.'.$modelName) : __('main._home')}}</p>
                 <p class="uk-margin-remove text-highlighted" style="font-size: 26px; font-weight:700">{{__($page_title)}}</p>
             </div>
         </div>

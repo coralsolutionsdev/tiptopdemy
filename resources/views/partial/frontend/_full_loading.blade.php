a3d8d0bc1bf1b456cd375dc4e2a6103e03b11857
Mehmet Munaf <moh.munaf@gmail.com>
2021-01-28 22:04:00 +0300

    apply white background for the full loading page

--- resources/views/partial/frontend/_full_loading.blade.php
+++ resources/views/partial/frontend/_full_loading.blade.php
@@ -1,5 +1,5 @@
 <section>
-    <div class="full-screen-spinner uk-flex uk-flex-center uk-flex-middle" style="position: fixed; top: 0px; z-index: 1000; width: 100%; height: 100vh;">
+    <div class="full-screen-spinner uk-flex uk-flex-center uk-flex-middle" style="position: fixed; top: 0px; z-index: 1000; width: 100%; height: 100vh; background-color: white">
         <div style="padding: 15px; background-color: rgba(255, 255, 255, 0.7); border-radius: 50%">
             @if(!empty(getSite()->logo))
             <img src="{{asset_image(getSite()->logo)}}" style="height: 50px; position: absolute; margin: 20px" alt="">

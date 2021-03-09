9872e4d3f6153d5ade46697420c307c0da92f497
Mehmet Munaf <moh.munaf@gmail.com>
2021-03-10 01:50:20 +0300

    [GroupItem] create groupItem model

---
+++ app/Modules/Group/GroupItem.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Modules\Group;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class GroupItem extends Model
+{
+    use SoftDeletes;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'title',
+        'description',
+        'hash_id',
+        'status',
+        'type',
+        'properties',
+        'group_id',
+        'creator_id',
+        'editor_id',
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'id' => 'integer',
+        'status' => 'integer',
+        'group_id' => 'integer',
+        'creator_id' => 'integer',
+        'editor_id' => 'integer',
+    ];
+
+
+    public function group()
+    {
+        return $this->belongsTo(\App\Modules\Group\Group::class);
+    }
+
+    public function creator()
+    {
+        return $this->belongsTo(\App\Modules\Group\User::class);
+    }
+
+    public function editor()
+    {
+        return $this->belongsTo(\App\Modules\Group\User::class);
+    }
+}
---
+++ database/migrations/2021_03_09_224818_create_group_items_table.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateGroupItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::disableForeignKeyConstraints();
+
+        Schema::create('group_items', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('title', 400);
+            $table->string('description', 400)->nullable();
+            $table->string('hash_id', 400)->nullable();
+            $table->tinyInteger('status')->nullable();
+            $table->integer('type')->nullable();
+            $table->text('properties')->nullable();
+            $table->unsignedBigInteger('group_id')->nullable();
+            $table->foreign('group_id')->references('id')->on('groups')->onDelete('cascade');
+            $table->unsignedBigInteger('creator_id')->nullable();
+            $table->foreign('creator_id')->references('id')->on('users')->onDelete('cascade');
+            $table->unsignedBigInteger('editor_id')->nullable();
+            $table->foreign('editor_id')->references('id')->on('users')->onDelete('cascade');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+
+        Schema::enableForeignKeyConstraints();
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('group_items');
+    }
+}

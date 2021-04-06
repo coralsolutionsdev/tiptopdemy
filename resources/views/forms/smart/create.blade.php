4579f3d7331f8b0f17a0d26c401f2e4f40998f32
Mehmet Munaf <moh.munaf@gmail.com>
2021-04-07 00:55:54 +0300

    [Form][Smart] bugfix

--- resources/views/forms/smart/create.blade.php
+++ resources/views/forms/smart/create.blade.php
@@ -9,7 +9,7 @@
             height: 100%;
         }
     </style>
-    <link rel="stylesheet" href="{{asset('themes/general/modules/css/at.css?v=202104071226')}}">
+    <link rel="stylesheet" href="{{asset('themes/general/modules/css/at.css?v=202104071230')}}">
 
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
@@ -19,6 +19,6 @@
         <smart-form-create current-unit-num="{{$unitNumber}}" current-lesson-num="{{$lessonNumber}}" lesson-slug="{{$lesson->slug}}"></smart-form-create>
     </div>
     @include('partial.scripts._tinyemc')
-    <script src="{{asset('js/app.js?v=202104071226')}}"></script>
+    <script src="{{asset('js/app.js?v=202104071230')}}"></script>
 
 @endsection
